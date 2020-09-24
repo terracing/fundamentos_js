4a7cdecbe367860e213614dd60cba23e93a17987
Omar Terrazas <terrazas.omar@gmail.com>
2020-09-23 23:19:12 -0500

    Clase 3, variables: números.

---
+++ assets/js/clase-03.js
@@ -0,0 +1,23 @@
+var edad = 35
+
+edad += 1
+
+var peso = 75
+
+peso -= 2
+
+var sandwich = 1
+ 
+peso += sandwich
+
+var jugarAlFutbol = 3
+
+peso -= jugarAlFutbol
+
+var precioDelVino = 200.3
+
+var total = Math.round(precioDelVino * 100 * 3) / 100
+
+var totalStr = total.toFixed(2)
+
+var totalFloat = parseFloat(totalStr)
