267f70bbcd84ac59c462466dbb450fbb8aba05c5
Omar Terrazas <terrazas.omar@gmail.com>
2020-09-23 23:01:07 -0500

    Clase 2, variables: string.

---
+++ assets/js/clase-02.js
@@ -0,0 +1,12 @@
+var nombre = 'Omar',
+    apellido = 'Terrazas'
+
+var nombreEnMayusculas = nombre.toUpperCase()
+var apellidoEnMinusculas = apellido.toLowerCase()
+
+var primeraLetraDelNombre = nombre.charAt()
+var cantidadDeLetrasDelNombre = nombre.length
+
+var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
+var str = `${nombre.charAt(1)} ${apellido.charAt(1)}`
+var substr = nombre.substr(1,3)
