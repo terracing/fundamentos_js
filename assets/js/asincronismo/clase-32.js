ee7bd7b5a5ac2e33eb02f8b3470a8bbed18af38f
Omar Terrazas <terrazas.omar@gmail.com>
2020-10-08 11:43:56 -0500

    Clase 32, promesas.

---
+++ assets/js/asincronismo/clase-32.js
@@ -0,0 +1,25 @@
+const API_URL = 'https://rickandmortyapi.com/api/'
+const CHARACTER_URL = 'character/:id'
+
+const opts = {
+    crossDomain: true
+}
+
+const obtenerPersonaje = id => {
+    return new Promise((resolve, reject) => {
+        let characterUrl = `${ API_URL }${ CHARACTER_URL.replace(':id',id) }`
+        $.get(characterUrl, opts, data => {
+            resolve(data)
+        }).fail(() => reject(id))
+    })
+}
+
+const onError = id => {
+    console.log(`Ocurrió un error al recuperar el personaje cin ID ${ id }`)
+}
+
+obtenerPersonaje(1)
+    .then(character => {
+        console.log(`Hola yo soy ${ character.name }`)
+    })
+    .catch(onError)
